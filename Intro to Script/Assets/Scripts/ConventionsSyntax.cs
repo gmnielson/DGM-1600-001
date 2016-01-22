39ff9c7394d4dbe7a30328c2f69f2a6ba6ca2d68
unknown <10543991@CS514-05.ad.uvu.edu>
2016-01-22 08:50:16 -0700

    ConventionsSyntax

---
+++ Intro to Script/Assets/Scripts/ConventionsSyntax.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConventionsSyntax : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log (transform.position.y);
+
+		if (transform.position.y <= 10f) {
+			Debug.Log ("Ahhhhh! I am falling!");
+		}
+
+		if (transform.position.y <= 6f) {
+			Debug.Log ("Bye, cruel world!");
+		}
+
+		if (transform.position.y <= 3f) {
+			Debug.Log ("I am about to hit the ground.");
+		}
+
+		if (transform.position.y <= -4f) {
+			Debug.Log ("Hmm? I am not dead yet.");
+		}
+
+		if (transform.position.y <= -7f) {
+			Debug.Log ("Wait...");
+		}
+
+		if (transform.position.y <= -11f) {
+			Debug.Log ("Oh, no! I am going to heck! Ahhhhhhh!!!!! HEEEEELLLLLLP!!!!!!!");
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Intro to Script/Assets/Scripts/Multiply.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Multiply : MonoBehaviour
+{
+	public int myInt = 10;
+
+	// Use this for initialization
+	void Start ()
+	{
+		myInt = MultiplyByTwo (myInt);
+		Debug.Log (myInt);
+	}
+
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
