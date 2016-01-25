82656a73d2e9cca66448565caf551d9e892605e7
unknown <10543991@CS514-05.ad.uvu.edu>
2016-01-25 08:15:18 -0700

    IfStatement script

--- Intro to Script/Assets/Scripts/conventionsandsyntaxpassoff.cs
+++ Intro to Script/Assets/Scripts/conventionsandsyntaxpassoff.cs
@@ -21,9 +21,12 @@ public class conventionsandsyntaxpassoff : MonoBehaviour
 		//The following statement will tell unity when to print "I'm going to splater!" to the console if the cube is < or = 10. 
 		//Enter the correct statement.
 
-		if (transform.) 
-		{
-			Debug.Log ();
+		if (transform.position.y <= 10f) {
+			Debug.Log ("I'm going to splater!");
+		}
+
+		else {
+			Debug.Log ("I am flying!");
 		}
 	}
 }
