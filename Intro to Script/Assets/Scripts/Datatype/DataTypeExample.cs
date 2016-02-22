68127767068144d35234ae937a7475cb880d3671
Gideon Max Nielson <elementals04@gmail.com>
2016-02-22 08:28:53 -0700

    Datatype

---
+++ Intro to Script/Assets/Scripts/Datatype/DataTypeExample.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataTypeExample : MonoBehaviour
+{
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			MoveUp ();
+
+		if(Input.GetKeyDown (KeyCode.RightAlt))
+			MoveDown ();
+	}
+
+
+	void MoveUp ()
+	{ //when space key is pressed the following happens.
+		Transform tran = transform;
+		tran.position = new Vector3(0, 5, 0);
+
+		Debug.Log ("Moved Up");
+	}
+
+	void MoveDown ()
+	{ //when right alt key is pressed the following happens.
+		Transform tran = transform;
+		tran.position = new Vector3 (0, 2, 0);
+
+		Debug.Log ("Moved Down");
+	}
+}
