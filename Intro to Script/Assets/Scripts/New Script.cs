aa66e339647fcec0bc6399476c532ee5b2939d96
Gideon Max Nielson <elementals04@gmail.com>
2016-02-22 08:56:31 -0700

    Gitflow

---
+++ Intro to Script/Assets/Scripts/New Script.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+			Debug.Log ("This is for pull request.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
