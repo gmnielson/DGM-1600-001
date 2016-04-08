89eaa8d98a79f0881ad90ace02dbb432d7a380c1
unknown <10543991@CS514-05.ad.uvu.edu>
2016-04-08 08:48:55 -0600

    Animator

---
+++ Intro to Script/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterScript : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponents<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis ("Vertical");
+		anim.SetFloat ("Speed", move);
+	}
+}
