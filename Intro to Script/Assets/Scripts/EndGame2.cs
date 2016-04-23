7cc54035ed21026a83ae48ab11c831b899b089ef
Gideon Max Nielson <elementals04@gmail.com>
2016-04-22 21:15:32 -0600

    Revert "???"
    This reverts commit 6d1fe70f088241618859fcbf71d36f9387c67756.

---
+++ Intro to Script/Assets/Scripts/EndGame2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame2 : MonoBehaviour {
+
+	public GameObject other;
+
+
+	void OnTriggerEnter (Collider Box)
+	{
+		if (Box.transform.tag == "Player")
+		{
+			Destroy (other);
+			Debug.Log ("Roller is defeated.");
+			Debug.Log ("You won!");
+		}
+	}
+}
--- Intro to Script/Assets/Scripts/Invoke.cs
+++ Intro to Script/Assets/Scripts/Invoke.cs
@@ -8,13 +8,14 @@ public class Invoke : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		Invoke ("SpawnObject", 90);
+		Invoke ("SpawnObject", 45);
 	}
 
 	void SpawnObject ()
 	{
 		float x = Random.Range (-0.01f, 0.01f);
 		float z = Random.Range (-0.01f, 0.01f);
-		Instantiate (target, new Vector3 (x, 0, z), Quaternion.identity);
+		Instantiate (target, new Vector3 (x, .025f, z), Quaternion.identity);
+		Debug.Log ("Home is at the center");
 	}
 }
